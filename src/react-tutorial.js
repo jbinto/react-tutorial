b27e47e5eee365aef8e6d20f46b39cba5bf8e768
Jesse Buchanan <hello@jessebuchanan.ca>
2015-09-22 14:56:57 -0400

    Finished "Component Properties"
    https://facebook.github.io/react/docs/tutorial.html#component-properties

--- src/react-tutorial.js
+++ src/react-tutorial.js
@@ -2,11 +2,47 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Jesse">This is a comment</Comment>
+        <Comment author="billg">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">{this.props.author}</h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
 
 React.render(
   <CommentBox />,
